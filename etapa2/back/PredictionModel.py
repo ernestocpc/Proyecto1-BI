2544e4861e9dc11bf40d441acccda0a561c5e6ab
Juan Nunez <j.nunezc@uniandes.edu.co>
2023-10-26 14:48:34 -0500

    feat: back medio funcionando

--- etapa2/back/PredictionModel.py
+++ etapa2/back/PredictionModel.py
@@ -2,8 +2,8 @@ from joblib import load
 
 class Model:
 
-    def __init__(self,columns):
-        self.model = load("etapa1\modeloCNB.joblib")
+    def __init__(self):
+        self.model = load("modeloCNB.joblib")
 
     def make_predictions(self, data):
         result = self.model.predict(data)
--- etapa2/back/main.py
+++ etapa2/back/main.py
@@ -1,6 +1,9 @@
+import pandas as pd
 from typing import Optional
-
 from fastapi import FastAPI
+from joblib import load
+from DataModel import DataModel
+from PredictionModel import Model
 
 app = FastAPI()
 
@@ -11,3 +14,12 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
    return {"item_id": item_id, "q": q}
+
+@app.post("/predict")
+def make_predictions(dataModel: DataModel):
+    texto = dataModel.dict()['text']
+    df = pd.DataFrame([texto], columns=['Textos_espanol'])
+    model = load("modeloCNB.joblib")
+    result = model.predict(df['Textos_espanol'])[0]
+    print(result)
+    return result
