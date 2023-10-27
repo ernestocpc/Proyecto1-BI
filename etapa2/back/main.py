30da94091dcc63f819507d226e9f353b860b9138
Juan Nunez <j.nunezc@uniandes.edu.co>
2023-10-27 10:30:04 -0500

    fix: back conectado con front

--- etapa2/back/main.py
+++ etapa2/back/main.py
@@ -3,9 +3,23 @@ from typing import Optional
 from fastapi import FastAPI
 from joblib import load
 from DataModel import DataModel
+from fastapi.middleware.cors import CORSMiddleware
 
+model = load("modeloCNB.joblib")
 app = FastAPI()
- 
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["POST"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def read_root():
    return {"Hello": "World"}
@@ -14,6 +28,14 @@ def read_root():
 def make_predictions(dataModel: DataModel):
     texto = dataModel.dict()['text']
     df = pd.DataFrame([texto], columns=['Textos_espanol'])
-    model = load("modeloCNB.joblib")
     result = model.predict(df['Textos_espanol'])[0]
-    return int(result)
+    probabilidades = model.predict_proba(df['Textos_espanol'])[0] # Probabilidades: [6, 7, 16]
+    jsonResultadoYProbabilidades = {
+        "resultado": int(result),
+        "probabilidades": {
+            "6": float(probabilidades[0]),
+            "7": float(probabilidades[1]),
+            "16": float(probabilidades[2])
+        }
+    }
+    return jsonResultadoYProbabilidades
--- etapa2/front/src/components/classifier.js
+++ etapa2/front/src/components/classifier.js
@@ -7,24 +7,30 @@ import Button from "react-bootstrap/Button";
 
 
 function Classifier() {
-    const [formValues, setFormValues] = useState({text:""})
-    const [label, setLabel] = useState(6)
+    const [formValues, setFormValues] = useState({ text: "" })
+    const [label, setLabel] = useState(-1)
 
-    const URL = ''
+    const URL = 'http://127.0.0.1:8000/predict'
 
     async function handlePost() {
-        const response = await fetch(URL, {method:"POST", body: JSON.stringify(formValues),  headers: {"Content-type":'application/json;charset=utf-8'}})
+        const response = await fetch(URL,
+            {
+                method: "POST",
+                body: JSON.stringify(formValues), 
+                headers: { "Content-type": 'application/json;charset=utf-8' }
+            })
         const data = await response.json()
-        setLabel(data)
+        console.log(data)
+        //setLabel(data)
     }
 
     const handleTextChange = (e) => {
-        setFormValues({...formValues, text: e.target.value})
+        setFormValues({ ...formValues, text: e.target.value })
     }
 
     const sendText = () => {
         if (formValues.text.length !== 0) {
-            //handlePost()
+            handlePost()
             console.log("Se realiza el envio :D")
         } else {
             alert("No ha ingresado ningun texto!!!")
@@ -32,29 +38,29 @@ function Classifier() {
     }
 
     const returnLabel = () => {
-        if(label===6){
-            return(<p>El texto ingresado se alinea con el ODS 6. Agua limpia y saneamiento</p>)
-        } else if (label === 7){
-            return(<p>El texto ingresado se alinea con el ODS 7. Energía Asequible y no contaminante </p>)
-        } else if (label ===16){
-            return(<p>El texto ingresado se alinea con el ODS 16. Paz, justicia e instituciones sólidas</p>)
+        if (label === 6) {
+            return (<p>El texto ingresado se alinea con el ODS 6. Agua limpia y saneamiento</p>)
+        } else if (label === 7) {
+            return (<p>El texto ingresado se alinea con el ODS 7. Energía Asequible y no contaminante </p>)
+        } else if (label === 16) {
+            return (<p>El texto ingresado se alinea con el ODS 16. Paz, justicia e instituciones sólidas</p>)
         }
     }
 
-    return(
+    return (
         <div>
             <Container>
                 <Form>
                     <Form.Group className='mb-2'>
                         <Form.Label><h2>Ingrese su texto</h2></Form.Label>
-                        <Form.Control as ="textarea" onChange={handleTextChange} value={formValues.text}/>
+                        <Form.Control as="textarea" onChange={handleTextChange} value={formValues.text} />
                     </Form.Group>
                     <Button variant='primary' onClick={sendText}>Clasificar</Button>
                 </Form>
             </Container>
             <Container>
                 <h2>Resultado de la clasificación</h2>
-                {(label != -1) && 
+                {(label != -1) &&
                     returnLabel()}
             </Container>
         </div>
