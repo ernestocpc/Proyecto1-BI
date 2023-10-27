8010a65284c2b7579501b53f994cd20f6c34c630
Juan Nunez <j.nunezc@uniandes.edu.co>
2023-10-26 20:47:39 -0500

    fix: pruebas joblib

--- etapa2/back/main.py
+++ etapa2/back/main.py
@@ -5,15 +5,14 @@ from joblib import load
 from DataModel import DataModel
 from PredictionModel import Model
 
-app = FastAPI()
+import Pipeline
+from Pipeline import TextTransformer
 
+app = FastAPI()
+ 
 @app.get("/")
 def read_root():
    return {"Hello": "World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-   return {"item_id": item_id, "q": q}
 
 @app.post("/predict")
 def make_predictions(dataModel: DataModel):
