f0f855efab3cf7ea778b7ae2966c9165a95bd89e
msgalvan1207 <85647232+msgalvan1207@users.noreply.github.com>
2023-10-29 02:42:38 -0500

    feat: indicador de ODS cambia dependiendo resultado

--- etapa2/front/src/components/classifier/classifier.js
+++ etapa2/front/src/components/classifier/classifier.js
@@ -1,4 +1,5 @@
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row"
 import Form from "react-bootstrap/Form";
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
@@ -53,11 +54,36 @@ function Classifier() {
 
     const returnLabel = () => {
         if (label === 6) {
-            return (<p>El texto ingresado se alinea con el ODS 6. Agua limpia y saneamiento</p>)
+            return (
+                <div>
+                    <Row><p>El texto ingresado se alinea con el <b className='text-wrap bg-6 rounded-2'>ODS 6. Agua limpia y saneamiento</b></p></Row>
+                    <Row className='text-center'><a href='https://www.un.org/sustainabledevelopment/es/water-and-sanitation/'><img className='img-fluid rounded' alt='ODS 6. Agua limpia y saneamiento' src='https://www.unicef.org/sites/default/files/styles/crop_thumbnail/public/S-WEB-Goal-06.png?itok=GELGcmKf'/></a></Row>
+                </div>
+            )
         } else if (label === 7) {
-            return (<p>El texto ingresado se alinea con el ODS 7. Energía Asequible y no contaminante </p>)
+            return (
+                <div>
+                    <Row><p>El texto ingresado se alinea con el <b className='text-wrap bg-7 rounded-2'>ODS 7. Energía Asequible y no contaminante</b> </p></Row>
+                    <Row className='text-center'><a href='https://www.un.org/sustainabledevelopment/es/energy/'><img className='img-fluid rounded' alt='ODS 7. Energía asequible y no contaminante' src='https://www.unicef.org/sites/default/files/styles/crop_thumbnail/public/S-WEB-Goal-07.png?itok=0xptJrFs'/></a></Row>
+                </div>
+            )
         } else if (label === 16) {
-            return (<p>El texto ingresado se alinea con el ODS 16. Paz, justicia e instituciones sólidas</p>)
+            return (
+                <div>
+                    <Row><p>El texto ingresado se alinea con el <b className='text-wrap bg-16 rounded-2'>ODS 16. Paz, justicia e instituciones sólidas</b></p></Row>
+                    <Row className='text-center'><a href='https://www.un.org/sustainabledevelopment/es/peace-justice/'><img className='img-fluid rounded' alt='ODS 16. Paz, justicia e instituciones solidas' src='https://www.unicef.org/sites/default/files/styles/crop_thumbnail/public/S-WEB-Goal-16.png?itok=zRRLXl8t'/></a></Row>
+                </div>
+            )
+        }
+    }
+
+    const containerBg = () => {
+        if (label===6) {
+            return ('bg-6 rounded-3 text-white')
+        } else if (label===7) {
+            return ('bg-7 rounded-3 text-white')
+        } else if (label===16){
+            return ('bg-16 rounded-3 text-white')
         }
     }
 
@@ -65,6 +91,9 @@ function Classifier() {
         <>
             <Container style={{ paddingTop: '10px', paddingBottom: '10px' }}>
                 <h2>Ingrese su texto</h2>
+                <Form.Text muted>
+                    Ingresa tu texto en aquí y haz click en clasificar para poder ver con que ODS se alinea
+                </Form.Text>
                 <Form.Group style={{ paddingTop: '10px', paddingBottom: '10px' }}>
                     <Form.Control as="textarea" onChange={handleTextChange} value={formValues.text} rows={8} />
                 </Form.Group>
@@ -74,7 +103,7 @@ function Classifier() {
             </Container>
             {label !== -1 && (
                 <>
-                    <Container style={{ paddingTop: '10px', paddingBottom: '10px' }}>
+                    <Container className={containerBg()} style={{ paddingTop: '10px', paddingBottom: '10px' }}>
                         <h2>Resultado de la clasificación</h2>
                         {returnLabel()}
                     </Container>
